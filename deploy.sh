9526f8cb292ab3fc319f97ad0f867dfdc841e1c1
Mikołaj Mikołajczak <nikelaos97@gmail.com>
2022-02-27 21:10:05 +0100

    Changed github url in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:MMikolajczak97/admin-panel.git main:gh-pages
+git push -f https://github.com/MMikolajczak97/Admin-Panel.git main:gh-pages
 
 cd -
